f9a20910e2fc89fa7c1501d8fdf3ac7701daab7a
adnanbezerra <adnanbezerra@proton.me>
2025-04-16 20:58:17 -0300

    feat/inquirer to select timespans

--- src/index.js
+++ src/index.js
@@ -5,16 +5,39 @@ import { resolve } from 'path';
 import { Worker } from 'worker_threads';
 import chalk from 'chalk';
 import fs from 'fs';
+import inquirer from 'inquirer';
 
-// const root = resolve(os.homedir(), 'Work');
 const root = process.cwd();
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
-const oneYearAgo = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000);
-
 const IGNORED_DIRECTORIES = ['.local', '.vscode'];
+
+const ONE_MONTH = 30 * 24 * 60 * 60 * 1000;
+
+const TIME_SPANS = [
+  {
+    name: '1 year',
+    value: 12 * ONE_MONTH
+  },
+  {
+    name: '9 months',
+    value: 9 * ONE_MONTH
+  },
+  {
+    name: '6 months',
+    value: 6 * ONE_MONTH
+  },
+  {
+    name: '3 months',
+    value: 3 * ONE_MONTH
+  },
+  {
+    name: '1 month',
+    value: ONE_MONTH
+  }
+]
 
 async function listSubdirs(dir) {
   const entries = await readdir(dir, { withFileTypes: true });
@@ -66,17 +89,29 @@ function parseDate(dateStr) {
     }))
 
   console.log(chalk.blue("--------------------------------"))
-  console.log(chalk.blue("Do you want to delete node_modules older than 1 year? (y/n)"));
+  console.log(chalk.blue("Do you want to delete node_modules? (y/n)"));
   console.log(chalk.blue("--------------------------------"));
 
-  process.stdin.once('data', (data) => {
+  process.stdin.once('data', async (data) => {
     const answer = data.toString().toLowerCase();
     
     if (answer === 'y') {
-      console.log(chalk.green('Deleting node_modules older than 1 year...'));            
+      const { timeSpan } = await inquirer.prompt({
+        type: 'list',
+        name: 'timeSpan',
+        message: 'Select time span',
+        choices: TIME_SPANS.map(timeSpan => ({
+          name: timeSpan.name,
+          value: timeSpan
+        }))
+      });
+
+      console.log(chalk.green(`Deleting node_modules older than ${timeSpan.name}...`));            
+      const now = new Date().getTime();
       flattened.forEach((found, index) => {
         const modifiedDate = parseDate(found.modified);
-        if (modifiedDate < oneYearAgo && !IGNORED_DIRECTORIES.some(ignored => found.path.includes(ignored))) {
+        const modifiedTime = modifiedDate.getTime();
+        if (modifiedTime < (now - timeSpan.value) && !IGNORED_DIRECTORIES.some(ignored => found.path.includes(ignored))) {
           console.log(`[${index + 1}] Deleting ${found.path}`);          
           fs.rmdirSync(found.path, { recursive: true });
         }
